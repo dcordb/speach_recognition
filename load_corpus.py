29d44d128b339ff0c9774a9777585fd140f4ce9f
Daniel Enrique <dcordb97@gmail.com>
2019-05-10 21:41:55 -0400

    Cambie un poco el load_corpus.py, puse que la mayor parte fuera para el train y el resto para test\nTambien le hice random shuffle al arreglo data

--- load_corpus.py
+++ load_corpus.py
@@ -1,5 +1,4 @@
 from scipy.io import wavfile
-# import nltk
 from nltk.corpus import PlaintextCorpusReader as pcr
 import csv
 from random import shuffle
@@ -10,28 +9,38 @@ wav_files = corpus_dir + '/wav48'
 
 def gen_cvs():
     my_corpus = pcr(corpus_dir, r'.*\.txt')
-    data = [['filename', 'transcript']]
+    data = [[]]
+
     for f_name in my_corpus.fileids():
         if 'txt/p' not in f_name:
             continue
         wav = wav_files + f_name[3:len(f_name)-4] + '.wav'
         data.append([wav, my_corpus.raw(f_name)[0:len(my_corpus.raw(f_name)) - 1]])
 
-    shuffle(data)
+    print('Processed', len(data), 'files')
+    print('Shuffling data')
+
+    shuffle(data) #suffle first the data
+    data.insert(0, ['filename', 'transcript']) #add the headers
+
+    print('Saving train.csv...')
 
     with open(corpus_dir + 'data/train.csv', 'w') as csv_train:
         writer = csv.writer(csv_train)
-        writer.writerows(data[:220])
+        writer.writerows([data[0]]+data[220:])
 
     csv_train.close()
 
+    print('Done')
+    print('Saving test.csv...')
+
     with open(corpus_dir + 'data/test.csv', 'w') as csv_test:
         writer = csv.writer(csv_test)
-        writer.writerows([data[0]]+data[220:])
+        writer.writerows(data[:220])
 
     csv_test.close()
 
+    print('Done')
+
 
 gen_cvs()
-
-# fs, data = wavfile.read('./output/audio.wav')
